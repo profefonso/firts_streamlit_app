7308855836429f210ac073f66a328e9f3813949e
Alfonso Caro Portillo <alfonsocarop@gmail.com>
2022-05-01 08:55:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit macros.txt")
-streamlit.dataframe(my fruit list)
+streamlit.dataframe(my_fruit_list)
